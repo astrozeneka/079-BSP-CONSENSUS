2f8785bab87c5e9fd94408ff9fc121afbcfd611c
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-02-09 09:39:07 +0700

    Update 106 to be both run on tara and local

--- 106_align_male_female.sh
+++ 106_align_male_female.sh
@@ -6,13 +6,20 @@
 #SBATCH -J BWAjob_ryan
 #SBATCH -A proj5034
 
-module purge
-module load bzip2/1.0.8-GCCcore-10.2.0
-module load ncurses/6.2-GCCcore-10.2.0
-module load foss/2021b
-module load Singularity/3.3.0
-source /tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/Softwares/cactus/venv/bin/activate
-export PATH=$PATH:/tarafs/data/project/proj5057-AGBKUB/24-ryan/Softwares/cactus/bin
+# if the --local flag is used
+if [ "$1" == "--local" ]; then
+  echo "Running locally"
+  source /mnt/extra/Softwares/cactus/venv/bin/activate
+else
+  echo "Running on slurm"
+  module purge
+  module load bzip2/1.0.8-GCCcore-10.2.0
+  module load ncurses/6.2-GCCcore-10.2.0
+  module load foss/2021b
+  module load Singularity/3.3.0
+  source /tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/Softwares/cactus/venv/bin/activate
+  export PATH=$PATH:/tarafs/data/project/proj5057-AGBKUB/24-ryan/Softwares/cactus/bin
+fi
 
 cat <<EOF > data/consensus.txt
 consensus_male ./data/downsampled/consensus-male.fa
